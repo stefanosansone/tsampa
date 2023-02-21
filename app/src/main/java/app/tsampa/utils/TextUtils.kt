67850cecad8b3e8b0743532237716525b232b109
Stefano Sansone <83456769+stefanosansone@users.noreply.github.com>
2023-02-21 01:41:56 +0100

    Add text utils for capitalization

---
+++ app/src/main/java/app/tsampa/utils/TextUtils.kt
@@ -0,0 +1,11 @@
+package app.tsampa.utils
+
+import java.util.Locale
+
+fun String.capitalize() : String {
+    return this.replaceFirstChar {
+        if (it.isLowerCase()) it.titlecase(
+            Locale.getDefault()
+        ) else it.toString()
+    }
+}
