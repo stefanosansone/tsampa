3a48a8d548970ee8cc53053c8279541f397ef3ad
Stefano Sansone <83456769+stefanosansone@users.noreply.github.com>
2023-02-23 09:56:58 +0100

    Add compose previews

--- app/src/main/java/app/tsampa/feature/diary/DiaryScreen.kt
+++ app/src/main/java/app/tsampa/feature/diary/DiaryScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.material.icons.filled.DonutLarge
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import app.tsampa.navigation.Route
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,4 +55,10 @@ private fun DiaryScreenTopBar(
             )
         }
     })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiaryScreenPreview() {
+    DiaryScreen(rememberNavController())
 }
--- app/src/main/java/app/tsampa/feature/nutrition/NutritionScreen.kt
+++ app/src/main/java/app/tsampa/feature/nutrition/NutritionScreen.kt
@@ -2,8 +2,15 @@ package app.tsampa.feature.nutrition
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 internal fun NutritionScreen() {
     Text("prova")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NutritionScreenPreview() {
+    NutritionScreen()
 }
